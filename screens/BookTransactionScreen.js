8819f33d0163d0a76244636f27e213daad9ee231
saniyaeram <53005024+saniyaeram@users.noreply.github.com>
2021-07-01 22:39:23 +0530

    Update BookTransactionScreen.js

--- screens/BookTransactionScreen.js
+++ screens/BookTransactionScreen.js
@@ -76,27 +76,7 @@ export default class TransactionScreen extends React.Component {
 
   initiateBookReturn = async ()=>{
     //add a transaction
-    db.collection("transactions").add({
-      'studentId' : this.state.scannedStudentId,
-      'bookId' : this.state.scannedBookId,
-      'date'   : firebase.firestore.Timestamp.now().toDate(),
-      'transactionType' : "Return"
-    })
-
-    //change book status
-    db.collection("books").doc(this.state.scannedBookId).update({
-      'bookAvailability' : true
-    })
-
-    //change book status
-    db.collection("students").doc(this.state.scannedStudentId).update({
-      'numberOfBooksIssued' : firebase.firestore.FieldValue.increment(-1)
-    })
-
-    this.setState({
-      scannedStudentId : '',
-      scannedBookId : ''
-    })
+    
   }
 
   handleTransaction = async()=>{
